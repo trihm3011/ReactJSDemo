d57571ca8924ea3d612734b738706af4697eb091
trihm3011 <tri.hoang@hotschedules.com>
2017-06-30 10:35:49 +0700

    - draft

--- src/views/Address/List/index.js
+++ src/views/Address/List/index.js
@@ -2,31 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { selectAddressListContainer } from './selectors';
-import { requestAddressesAjax } from './actions';
+import { requestAddressesAjax, requestAddressesSucceeded } from './actions';
 import { Table } from './components';
 
 class AddressListContainer extends Component {
-    
-    constructor(props) {
-        super(props);
-        
-    }
-    
-
     componentDidMount() {
         this.props.requestAddresses();
     }
 
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     console.log('nextProps.addresses', nextProps.addresses);
-    //     console.log('this.props.addresses', this.props.addresses);
-    //     console.log(JSON.stringify(nextProps.addresses) !== JSON.stringify(this.props.addresses));
-    //     return JSON.stringify(nextProps.addresses) !== JSON.stringify(this.props.addresses);
-    // }
+    shouldComponentUpdate(nextProps, nextState) {
+        return JSON.stringify(nextProps.addresses) !== JSON.stringify(this.props.addresses);
+    }
+
+    componentWillUnmount() {
+        this.props.cleanAddresses();
+    }
 
     render() {
         const { addresses } = this.props;
-        console.log('addresses', addresses);
         return (
             <div>
                 {addresses.length ? <Table addresses={addresses} /> : null}
@@ -38,13 +31,8 @@ class AddressListContainer extends Component {
 const mapDispatchToProps = (dispatch) => {
     return {
         requestAddresses: () => dispatch(requestAddressesAjax()),
+        cleanAddresses: () => dispatch(requestAddressesSucceeded([])),
     };
 };
 
 export default connect(selectAddressListContainer, mapDispatchToProps)(AddressListContainer);
-// export default connect((state) => {
-//     console.log('state', state);
-//     return {
-//         addresses: state.addressList
-//     };
-// }, mapDispatchToProps)(AddressListContainer);
